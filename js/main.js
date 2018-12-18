6527378d7ff844f828775e4145c692f019b83f30
Faneste <faneste12345@gmail.com>
2018-12-18 01:35:52 +0100

    dodat scrollintoview i celokupan meni

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 //////////// Slider Gallery
 
-var slideIndex = 1;
+let slideIndex = 1;
 showDivs(slideIndex);
 
 function plusDivs(n) {
@@ -8,8 +8,8 @@ function plusDivs(n) {
 }
 
 function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("slides");
+  let i;
+  let x = document.getElementsByClassName("slides");
   if (n > x.length) {slideIndex = 1}
   if (n < 1) {slideIndex = x.length}
   for (i = 0; i < x.length; i++) {
@@ -20,7 +20,25 @@ function showDivs(n) {
 
 //////////// End Slider Gallery
 
+//////////// Scroll Into View Menu
 
+let dorucak = document.getElementById("dorucak-menu-scroll");
+let hladnoPredjelo = document.getElementById("hladno-predjelo-menu-scroll");
+let toploPredjelo = document.getElementById("toplo-predjelo-menu-scroll");
+let salate = document.getElementById("salate-menu-scroll");
+let obrokSalate = document.getElementById("obrok-salate-menu-scroll");
+let pice = document.getElementById("pizze-menu-scroll");
+let paste = document.getElementById("paste-menu-scroll");
+
+function dorucakScroll(){ dorucak.scrollIntoView({ behavior: 'smooth' })}
+function hladnoPredjeloScroll(){ hladnoPredjelo.scrollIntoView({ behavior: 'smooth' })}
+function toploPredjeloScroll(){ toploPredjelo.scrollIntoView({ behavior: 'smooth' })}
+function salateScroll(){ salate.scrollIntoView({ behavior: 'smooth' })}
+function obrokSalateScroll(){ obrokSalate.scrollIntoView({ behavior: 'smooth' })}
+function piceScroll(){ pice.scrollIntoView({ behavior: 'smooth' })}
+function pasteScroll(){ paste.scrollIntoView({ behavior: 'smooth' })}
+
+//////////// End Scroll Into View Menu
 
 
 
